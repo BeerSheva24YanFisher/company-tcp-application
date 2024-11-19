3f4fbcc21ff7527a3e2b8e8307aa7653d4e83e4c
yanfi-sher <loss.soul20@gmail.com>
2024-11-19 15:29:48 +0200

    hw 41 (version Yuri)

--- company-server-appl/src/main/java/telran/employees/MainServer.java
+++ company-server-appl/src/main/java/telran/employees/MainServer.java
@@ -1,18 +1,22 @@
 package telran.employees;
 
+import telran.io.PersistableSaverThread;
 import telran.net.TcpServer;
 
 public class MainServer {
+    private static final String FILE_NAME = "employees.data";
     private static final int PORT = 4000;
-
+    private static final long TIME_INTERVAL = 60000;
+                
     public static void main(String[] args) {
         Company company = new CompanyImpl();
-        TcpServer server = new TcpServer(new CompanyProtocol(company), PORT);
         if (company instanceof Persistable persistable) {
-            persistable.restoreFromFile(Config.FILE_NAME);
-            SaveThread autoSaveThread = new SaveThread(persistable);
-            autoSaveThread.start();
-        }
-        server.run();
+            persistable.restoreFromFile(FILE_NAME);
+            PersistableSaverThread saverThread = new PersistableSaverThread(persistable, FILE_NAME, TIME_INTERVAL);
+            saverThread.start();
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> persistable.saveToFile(FILE_NAME)));
+        } 
+        TcpServer tcpServer = new TcpServer(new CompanyProtocol(company), PORT);
+        tcpServer.run();
     }
 }
